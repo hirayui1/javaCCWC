b7db3452eaa7df6d8e7f8a0e280d553a5ffa7dac
hirayui1 <playerhasbeentoastedby@gmail.com>
2024-04-25 16:16:23 +0200

    reads from cmd and stdinput by cat tool now

---
+++ ccwc.bat
@@ -0,0 +1,2 @@
+@echo off
+java -jar C:\javaCCWC\build\libs\javaCCWC-1.0-SNAPSHOT.jar %1 %2 %3 %4
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,81 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        switch (args[0]) {
-            case "-c" -> System.out.println(readBytes(args) + " " + args[1]);
-            case "-l" -> System.out.println(readLines(args) + " " + args[1]);
-            case "-w" -> System.out.println(readWordCount(args) + " " + args[1]);
-            default -> System.out.println("Unsupported CLI command.");
-        }
+
+        boolean noCommand = false;
+        if (args.length > 0 && args.length <= 2) {
+            String fileName;
+            if (args[0].endsWith(".txt")) {
+                fileName = args[0];
+                noCommand = true;
+            } else if (args.length > 1 && args[0].contains("-") && args[1].endsWith(".txt")) {
+                fileName = args[1];
+            } else {
+                fileName = "";
+            }
+
+            if (noCommand) {
+                System.out.println("\t" +
+                        readBytes(args) +
+                        " " +
+                        readLines(args) +
+                        " " +
+                        readWordCount(args) +
+                        " " +
+                        args[0]);
+            } else {
+                switch (args[0]) {
+                    case "-c" ->
+                            System.out.println("\t" + readBytes(args) + " " + fileName);
+                    case "-l" ->
+                            System.out.println("\t" + readLines(args) + " " + fileName);
+                    case "-w" ->
+                            System.out.println("\t" + readWordCount(args) + " " + fileName);
+                    case "-m" ->
+                            System.out.println("\t" + readCharacterCount(args) + " " + fileName);
+
+                    default ->
+                            System.out.println("Unsupported CLI command. Program usage: ccwc {command} {fileName.txt}");
+                }
+            }
+        } else System.out.println("Unsupported CLI command. Program usage: ccwc {command} {fileName.txt}");
     }
+
+    // decides if stdin or txt or no txt
+    private static FileInputStream getFileInputStream(String[] args) throws IOException {
+        if (args.length >= 2 && args[0].contains("-") && args[1].endsWith(".txt")) {
+            return new FileInputStream(args[1]);
+        } else if (args[0].endsWith(".txt")) {
+            return new FileInputStream(args[0]);
+        } else return new FileInputStream(FileDescriptor.in);
+    }
+
+    // decides if stdin or txt or no txt
+    private static BufferedReader getBufferedReader(String[] args) throws IOException {
+        if (args.length >= 2 && args[0].contains("-") && args[1].endsWith(".txt")){
+            return new BufferedReader(new InputStreamReader(new FileInputStream(args[1])));
+        } else if (args[0].endsWith(".txt")) {
+            return new BufferedReader(new InputStreamReader(new FileInputStream(args[0])));
+        } else return new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    private static int readCharacterCount(String[] args) {
+
+        int charCount = 0;
+        try (BufferedReader br = getBufferedReader(args)) {
+            int chars;
+            while ((chars = br.read()) != -1) {
+                charCount++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return charCount;
+    }
+
     private static int readWordCount(String[] args) {
         int wordCount = 0;
-        try (BufferedReader br = new BufferedReader(new FileReader(args[1]))) {
+        try (BufferedReader br = getBufferedReader(args)) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] words = line.split("\\s+");
@@ -29,7 +94,7 @@ public class Main {
 
     private static int readLines(String[] args) {
         int lineCount = 0;
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(args[1])))) {
+        try (BufferedReader br = getBufferedReader(args)) {
             while (br.readLine() != null) {
                 lineCount++;
             }
@@ -42,9 +107,9 @@ public class Main {
     // read and print each line into a String array
     public static int readBytes(String[] args) {
         int byteCount = 0;
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(args[1])))){
+        try (FileInputStream fil = getFileInputStream(args)) {
             int nextByte;
-            while((nextByte = br.read()) != -1) {
+            while ((nextByte = fil.read()) != -1) {
                 byteCount++;
             }
         } catch (IOException e) {
